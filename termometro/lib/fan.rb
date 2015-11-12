984d5b129a60941da9f6fa5aa90edbe30fd580ac
Ederson de Lima <edersondelima@gmail.com>
2015-11-12 00:13:20 -0200

    queue to notifications

--- termometro/lib/fan.rb
+++ termometro/lib/fan.rb
@@ -20,7 +20,14 @@ class Fan
   def self.put_queue
     client = Client.build
     queue  = Configuration.load['termometro']['send_queue']
+    notify client
     client.publish(queue, "#{on?}")
     puts " Ventilador: #{on?}".magenta
+  end
+
+  def self.notify(client)
+    queue   = Configuration.load['jaiminho']['queue']
+    message = Configuration.load['jaiminho']['message_fan']
+    client.publish(queue, message)
   end
 end
